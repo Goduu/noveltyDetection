aaffb3e30f65a8ca94816e9546a69f9d4cc7b02c
Cangussu <igor.cangussu@siemens.com>
2020-09-22 10:23:52 -0300

    new file:   Consumption.py 	new file:   data_preparation.py 	new file:   db_conexion.py 	new file:   main.py 	new file:   model_training.py 	new file:   noveltyDetection/.gitignore 	new file:   noveltyDetection/README.md 	new file:   prediction.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from data_preparation import execute_extraction
+from model_training import train_all
+from prediction import predict,predict2, evaluate,evaluate_cstm
+
+
+# execute_extraction(4,1000000)
+predict()
+
+# train_all()
